1dc6e3ff1448190b6602976acb442f0f4016797c
libienz <leekh10195@gmail.com>
2024-05-14 17:03:40 +0900

    test: ArgumentResolver 및 HandlerInterceptor 테스트 작성

--- src/test/java/roomescape/member/fixture/MemberFixture.java
+++ src/test/java/roomescape/member/fixture/MemberFixture.java
@@ -7,5 +7,6 @@ public class MemberFixture {
     public static final Member MEMBER_ID_1 = new Member(1L, "썬", "aa@gmail.com", "12", Role.MEMBER);
     public static final Member MEMBER_ID_2 = new Member(2L, "리비", "aa@gmail.com", "12", Role.MEMBER);
     public static final Member MEMBER_ID_3 = new Member(3L, "도도", "aa@gmail.com", "12", Role.MEMBER);
-
+    public static final Member ADMIN_MEMBER = new Member(4L, "어드민", "aa@gmail.com", "12", Role.ADMIN);
+    public static final Member NON_ADMIN_MEMBER = new Member(5L, "어드민", "aa@gmail.com", "12", Role.MEMBER);
 }
